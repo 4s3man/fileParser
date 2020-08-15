ad5db4aeee06a8f06cda172060bf5a65c1880dd8
Jakub Kułaga <kuba.kulaga.sv7@gmail.com>
2020-08-15 19:12:11 +0100

    Fix

--- main.py
+++ main.py
@@ -9,20 +9,28 @@ output = './output.csv'
 inputFile = './postBaldronContacts-withMamut.csv'#'./sample.csv'
 
 def processCell(value)-> str:
-    return fix_text(value)
+    if not isinstance(value, str):
+        return value
+    value = fix_text(value)
+    
+    print(value)
+
+    return value
 
 def processCity(city: str)-> str:
+    if not isinstance(city, str):
+        return city
+    
     return city.capitalize()
 
 def processRow(frame: DataFrame)-> DataFrame:
     for colName in frame.columns:
-        if frame[colName].dtype != str:
-            continue
-
         frame[colName] = frame[colName].map(processCell)
         
         if 'city' in colName or 'City' in colName:
             frame[colName] = frame[colName].map(processCity)
+        
+
         
     return frame    
 
@@ -31,10 +39,10 @@ def removeFileIfExists(output)-> None:
         os.remove(output)
 
 todoRemoveStop = 0
-chunksize = 10 ** 6
+chunksize = 10 ** 100
 for chunk in pd.read_csv(inputFile, chunksize=chunksize, encoding='iso-8859-1'):
     removeFileIfExists(output)
-    processRow(chunk).to_csv('./output.csv')
+    processRow(chunk).to_csv('./output.csv', index=False)
 
     # todoRemoveStop+=1
     # if todoRemoveStop > 4:
